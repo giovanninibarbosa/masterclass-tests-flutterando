b6991deaaebaaa49fed9e7f01c30ad3c958eb7a3
Giovannini Barbosa <giovanninibaarbosa@gmail.com>
2024-09-25 13:52:45 -0300

    style: group tests by type.

--- tests_api/test/imc_dart_test.dart
+++ tests_api/test/imc_dart_test.dart
@@ -9,11 +9,13 @@ void main() {
     expect(result, equals(20.199469763918696));
   });
 
-  test('Deve lançar uma excessão se altura for menor que 1', () {
-    expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
-  });
+  group('Excessões de parametros |', () {
+    test('Deve lançar uma excessão se altura for menor que 1', () {
+      expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
+    });
 
-  test('Deve lançar uma excessão se o peso for menor que 1', () {
-    expect(() => calcIMC(1.70, 0), throwsA(isA<Exception>()));
+    test('Deve lançar uma excessão se o peso for menor que 1', () {
+      expect(() => calcIMC(1.70, 0), throwsA(isA<Exception>()));
+    });
   });
 }
