e94bd3d8933d8b030197f08dd2c3d5e63a2c322b
Giovannini Barbosa <giovanninibaarbosa@gmail.com>
2024-09-25 13:48:45 -0300

    test: adds initial tests for imc.

--- tests_api/test/imc_dart_test.dart
+++ tests_api/test/imc_dart_test.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'package:test/test.dart';
+
+import '../imc.dart';
+
+void main() {
+  test('Deve efetuar o calculo do IMC', () {
+    final result = calcIMC(1.78, 64);
+
+    expect(result, equals(20.199469763918696));
+  });
+
+  test('Deve lançar uma excessão se altura for menor que 1', () {
+    expect(() => calcIMC(0, 64), throwsA(isA<Exception>()));
+  });
+
+  test('Deve lançar uma excessão se o peso for menor que 1', () {
+    expect(() => calcIMC(1.70, 0), throwsA(isA<Exception>()));
+  });
+}
